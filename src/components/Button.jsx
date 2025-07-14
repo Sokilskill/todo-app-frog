b0073bdc6017d6133d1aebb7f675319499dd0550
sokilskills <vprovorotov@amil.com>
2025-07-14 18:38:39 +0300

    ref: change size

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -10,7 +10,7 @@ const variants = {
 
 const sizes = {
   sm: "text-sm px-3 py-1.5",
-  md: "text-base px-4 py-2 md:w-full md:max-w-[90px]",
+  md: "text-base px-4 py-2",
   lg: "text-lg px-5 py-3",
   icon: "w-7 h-7 p-2",
 };
